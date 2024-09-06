952fd7c066f2fa7e6f67048b0ee74e2b6e7707c2
CoelhoGB <coelhogabriel03@gmail.com>
2024-09-05 21:35:01 -0300

    Commit

---
+++ cor/src/main/java/com/cor/Server.java
@@ -0,0 +1,11 @@
+package com.cor;
+import java.util.Scanner;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bem vindo(a) ao sistema!\nInsira seu email para começarmos. (Padrão: exemplo@email.com)" );
+    }
+}
